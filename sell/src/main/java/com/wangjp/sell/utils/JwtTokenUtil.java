247b9a5b0f9efeb6d9fbc804fa4f2c45374c653c
wangjp <943375372@qq.com>
2021-09-30 17:28:42 +0800

    redis 过期

--- sell/src/main/java/com/wangjp/sell/utils/JwtTokenUtil.java
+++ sell/src/main/java/com/wangjp/sell/utils/JwtTokenUtil.java
@@ -2,6 +2,7 @@ package com.wangjp.sell.utils;
 
 import cn.hutool.core.util.StrUtil;
 import com.wangjp.sell.config.JwtConfig;
+import com.wangjp.sell.constant.RedisConstant;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -16,6 +17,7 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wangjp
@@ -39,7 +41,6 @@ public class JwtTokenUtil {
      */
     public String createJwt(UserDetails userDetails) {
         SecretKey secretKey = new SecretKeySpec(jwtConfig.getSecret().getBytes(), SignatureAlgorithm.HS512.getJcaName());
-
         String token = Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date())
@@ -47,7 +48,36 @@ public class JwtTokenUtil {
                 .signWith(secretKey)
                 .compact();
 
+        // 生成 redis 用于控制 jwt 的有效时间
+        redisTemplate.opsForValue().set(getRedisKeyFromToken(token), userDetails, jwtConfig.getExpiration(), TimeUnit.SECONDS);
+
         return token;
+    }
+
+    /**
+     * 从 token 中获取 JWT 中的负载
+     */
+    private Claims getClaimsFromToken(String token) {
+        Claims claims = null;
+
+        // 验证 jwt 的有效时间
+        UserDetails userDetails = (UserDetails) redisTemplate.opsForValue().get(getRedisKeyFromToken(token));
+        if (userDetails == null) {
+            throw new RuntimeException();
+        }
+
+        try {
+            SecretKey secretKey = new SecretKeySpec(jwtConfig.getSecret().getBytes(), SignatureAlgorithm.HS512.getJcaName());
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        }catch (Exception e) {
+            System.out.println("JWT格式验证失败: " + token);
+        }
+
+        return claims;
     }
 
     /**
@@ -58,23 +88,10 @@ public class JwtTokenUtil {
     }
 
     /**
-     * 从 token 中获取 JWT 中的负载
+     * 根据 token 获取 redisKey
      */
-    private Claims getClaimsFromToken(String token) {
-        Claims claims = null;
-        SecretKey secretKey = new SecretKeySpec(jwtConfig.getSecret().getBytes(), SignatureAlgorithm.HS512.getJcaName());
-
-        try {
-            claims = Jwts.parserBuilder()
-                    .setSigningKey(secretKey)
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-        } catch (Exception e) {
-            System.out.println("JWT格式验证失败: " + token);
-        }
-
-        return claims;
+    private String getRedisKeyFromToken(String token) {
+        return RedisConstant.userCache + token;
     }
 
     /**
